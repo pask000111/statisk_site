47aaea1e89fd6b496a342bf77994f5982255f2e3
pask000111 <pask0001@stud.kea.dk>
2025-01-31 09:05:03 +0100

    mainændring

--- produkt.js
+++ produkt.js
@@ -3,8 +3,10 @@ let productContainer = document.querySelector(".product_container");
 
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
-  .then((data) => {
-    productContainer.innerHTML = `
+  .then((data) => showProduct(data));
+
+function showProduct(data) {
+  productContainer.innerHTML = `
         <div class="product_image">
           <img class="nikebluse" src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="nikebluse" />
         </div>
@@ -36,4 +38,4 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
             <button>Add to basket</button>
           </form>
         </div>`;
-  });
+}
